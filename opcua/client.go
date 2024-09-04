adb469ef6e319096f32a99ecf2ca308ad16ca24f
ZhangJian He <shoothzj@gmail.com>
2024-09-04 21:22:21 +0800

    feat: add empty client server
    Signed-off-by: ZhangJian He <shoothzj@gmail.com>

---
+++ opcua/client.go
@@ -0,0 +1,12 @@
+package opcua
+
+type ClientConfig struct {
+}
+
+type Client struct {
+}
+
+func NewClient(config *ClientConfig) *Client {
+	client := &Client{}
+	return client
+}
---
+++ opcua/server.go
@@ -0,0 +1,12 @@
+package opcua
+
+type ServerConfig struct {
+}
+
+type Server struct {
+}
+
+func NewServer(config *ServerConfig) *Server {
+	server := &Server{}
+	return server
+}
