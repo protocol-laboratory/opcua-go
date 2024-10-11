167310ca39347b994db19763f6bf05e4a0e3e748
alkibal <2573104964@qq.com>
2024-10-11 22:47:54 +0800

    feat: add the interface and unit test cases for encoder

---
+++ opcua/enc/encoder.go
@@ -0,0 +1,225 @@
+package enc
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"reflect"
+	"strings"
+
+	"opcua-go/opcua/uamsg"
+)
+
+var ErrValueIsNil = errors.New("value is nill")
+
+type Encoder interface {
+	Encode(v *uamsg.Message, chunkSize int) ([][]byte, error)
+	SetSequenceNumberGenerator(func() uint32)
+}
+
+// FastEncoder Performance-sensitive structures need to implement fast encoder
+type FastEncoder interface {
+	FastEncode() ([]byte, error)
+}
+
+type DefaultEncoder struct {
+	sequenceNumberGenerator func() uint32
+}
+
+func NewDefaultEncoder() *DefaultEncoder {
+	return &DefaultEncoder{}
+}
+
+func (e *DefaultEncoder) Encode(v *uamsg.Message, chunkSize int) ([][]byte, error) {
+	chunks := make([][]byte, 0)
+
+	var (
+		messageHeaderLength  int
+		securityHeaderLength int
+		sequenceHeaderLength int
+
+		securityHeaderBytes []byte
+		sequenceHeaderBytes []byte
+	)
+
+	switch v.MessageType {
+	case uamsg.HelloMessageType, uamsg.AcknowledgeMessageType:
+		messageHeaderLength = 3 + 1 + 4
+		securityHeaderLength = 0
+		sequenceHeaderLength = 0
+	case uamsg.OpenSecureChannelMessageType, uamsg.MsgMessageType, uamsg.CloseSecureChannelMessageType:
+		var err error
+		securityHeaderBytes, err = genericEncoder(v.SecurityHeader)
+		if err != nil {
+			return nil, err
+		}
+		messageHeaderLength = 3 + 1 + 4 + 4
+		securityHeaderLength = len(securityHeaderBytes)
+		sequenceHeaderLength = 8
+	default:
+		return nil, errors.New("not support message type")
+	}
+
+	dataBytes, err := genericEncoder(v.MessageBody)
+	if err != nil {
+		return nil, err
+	}
+
+	leftBodySize := len(dataBytes)
+	headerLength := messageHeaderLength + securityHeaderLength + sequenceHeaderLength
+
+	for leftBodySize > 0 {
+		tempBuff := bytes.NewBuffer(nil)
+		leftMsgSize := leftBodySize + headerLength
+		if leftMsgSize > chunkSize {
+			v.MessageHeader.ChunkType = uamsg.IntermediateChunkType
+			v.MessageHeader.MessageSize = uint32(chunkSize)
+		} else {
+			v.MessageHeader.ChunkType = uamsg.FinalChunkType
+			v.MessageHeader.MessageSize = uint32(leftMsgSize)
+		}
+		headerBytes, err := genericEncoder(v.MessageHeader)
+		if err != nil {
+			return nil, err
+		}
+		err = binary.Write(tempBuff, binary.LittleEndian, headerBytes)
+		if err != nil {
+			return nil, err
+		}
+		if v.MessageHeader.MessageType != uamsg.HelloMessageType && v.MessageHeader.MessageType != uamsg.AcknowledgeMessageType {
+			err = binary.Write(tempBuff, binary.LittleEndian, securityHeaderBytes)
+			if err != nil {
+				return nil, err
+			}
+
+			if e.sequenceNumberGenerator != nil {
+				v.SequenceHeader.SequenceNumber = e.sequenceNumberGenerator()
+			}
+			sequenceHeaderBytes, err = genericEncoder(v.SequenceHeader)
+			if err != nil {
+				return nil, err
+			}
+			err = binary.Write(tempBuff, binary.LittleEndian, sequenceHeaderBytes)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		if v.MessageHeader.ChunkType == uamsg.IntermediateChunkType {
+			writeBody := dataBytes[:chunkSize-headerLength]
+			dataBytes = dataBytes[chunkSize-headerLength:]
+			leftBodySize = len(dataBytes)
+			err = binary.Write(tempBuff, binary.LittleEndian, writeBody)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			leftBodySize = 0
+			err = binary.Write(tempBuff, binary.LittleEndian, dataBytes)
+			if err != nil {
+				return nil, err
+			}
+		}
+		chunks = append(chunks, tempBuff.Bytes())
+	}
+	return chunks, nil
+}
+
+func genericEncoder(v interface{}) ([]byte, error) {
+	buff := bytes.NewBuffer(nil)
+
+	value := reflect.ValueOf(v)
+	if value.Kind() == reflect.Ptr {
+		if value.IsNil() {
+			return nil, ErrValueIsNil
+		}
+		value = reflect.Indirect(value)
+	}
+
+	switch value.Kind() {
+	case reflect.Uint8, reflect.Uint32, reflect.Uint64, reflect.Int32, reflect.Int64, reflect.Float32, reflect.Float64:
+		err := binary.Write(buff, binary.LittleEndian, value.Interface())
+		if err != nil {
+			return nil, err
+		}
+		return buff.Bytes(), nil
+	case reflect.String:
+		strBytes, err := StringEncoder(value.Interface())
+		if err != nil {
+			return nil, err
+		}
+		return strBytes, nil
+	case reflect.Slice:
+		if value.Type().Elem().String() == reflect.Uint8.String() {
+			// byte slice fast path
+			valueBytes, err := ByteStringEncoder(value.Interface())
+			if err != nil {
+				return nil, err
+			}
+			return valueBytes, nil
+		}
+
+		length := int32(value.Len())
+		if length == 0 && value.IsNil() {
+			length = -1
+		}
+		err := binary.Write(buff, binary.LittleEndian, length)
+		if err != nil {
+			return nil, err
+		}
+
+		for i := 0; i < value.Len(); i++ {
+			elemBytes, err := genericEncoder(value.Index(i).Interface())
+			if err != nil {
+				return nil, err
+			}
+			buff.Write(elemBytes)
+		}
+
+	case reflect.Struct:
+		if value.Type().Implements(reflect.TypeOf((*FastEncoder)(nil)).Elem()) {
+			// fast path for all struct which implements FastEncoder
+			v, _ := value.Interface().(FastEncoder)
+			dataBytes, err := v.FastEncode()
+			if err != nil {
+				return nil, err
+			}
+			buff.Write(dataBytes)
+			return buff.Bytes(), nil
+		}
+		encoder, ok := SpecialStructEncoderMap[value.Type().Name()]
+		if !ok {
+			for i := 0; i < value.NumField(); i++ {
+				elemBytes, err := genericEncoder(value.Field(i).Interface())
+				if err != nil {
+					if !errors.Is(err, ErrValueIsNil) {
+						return nil, err
+					}
+					// enc: omitempty标记的值解不出来可以不加入编码，不算错误
+					tagValue, ok := value.Type().Field(i).Tag.Lookup("enc")
+					if !ok {
+						return nil, err
+					}
+					if !strings.Contains(tagValue, "omitempty") {
+						return nil, err
+					}
+					continue
+				}
+				buff.Write(elemBytes)
+			}
+		} else {
+			valueBytes, err := encoder(value.Interface())
+			if err != nil {
+				return nil, err
+			}
+			return valueBytes, nil
+		}
+	default:
+		err := binary.Write(buff, binary.LittleEndian, value.Interface())
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return buff.Bytes(), nil
+}
---
+++ opcua/enc/special_encoder.go
@@ -0,0 +1,586 @@
+package enc
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"reflect"
+
+	"opcua-go/opcua/uamsg"
+)
+
+func StringEncoder(v interface{}) ([]byte, error) {
+	buff := bytes.NewBuffer(nil)
+	value, ok := v.(string)
+	if !ok {
+		return nil, fmt.Errorf("error type of string, %v", reflect.TypeOf(v).Name())
+	}
+	length := int32(len(value))
+	if length == 0 {
+		length = -1
+	}
+	err := binary.Write(buff, binary.LittleEndian, length)
+	if err != nil {
+		return nil, err
+	}
+	buff.WriteString(value)
+	return buff.Bytes(), nil
+}
+
+func ByteStringEncoder(v interface{}) ([]byte, error) {
+	buff := bytes.NewBuffer(nil)
+	value, ok := v.([]byte)
+	if !ok {
+		return nil, fmt.Errorf("error type of bytestring, %v", reflect.TypeOf(v).Name())
+	}
+	// ByteString's empty and null encodings are inconsistent
+	length := int32(len(value))
+	if length == 0 && value == nil {
+		length = -1
+	}
+	err := binary.Write(buff, binary.LittleEndian, length)
+	if err != nil {
+		return nil, err
+	}
+	buff.Write(value)
+	return buff.Bytes(), nil
+}
+
+func GuidEncoder(v interface{}) ([]byte, error) {
+	buff := bytes.NewBuffer(nil)
+	var guid uamsg.Guid
+	temp, ok := v.(*uamsg.Guid)
+	if !ok {
+		guid, ok = v.(uamsg.Guid)
+		if !ok {
+			return nil, fmt.Errorf("value has wrong type, should be guid")
+		}
+		temp = &guid
+	}
+	guid = *temp
+	err := binary.Write(buff, binary.LittleEndian, guid.Data1)
+	if err != nil {
+		return nil, err
+	}
+	err = binary.Write(buff, binary.LittleEndian, guid.Data2)
+	if err != nil {
+		return nil, err
+	}
+	err = binary.Write(buff, binary.LittleEndian, guid.Data3)
+	if err != nil {
+		return nil, err
+	}
+	err = binary.Write(buff, binary.LittleEndian, guid.Data4)
+	if err != nil {
+		return nil, err
+	}
+	return buff.Bytes(), nil
+}
+
+func QualifiedNameEncoder(v interface{}) ([]byte, error) {
+	buff := bytes.NewBuffer(nil)
+	var qualifiedName uamsg.QualifiedName
+	temp, ok := v.(*uamsg.QualifiedName)
+	if !ok {
+		qualifiedName, ok = v.(uamsg.QualifiedName)
+		if !ok {
+			return nil, fmt.Errorf("value has wrong type, should be qualified name")
+		}
+		temp = &qualifiedName
+	}
+	qualifiedName = *temp
+	err := binary.Write(buff, binary.LittleEndian, qualifiedName.NamespaceIndex)
+	if err != nil {
+		return nil, err
+	}
+	tempBytes, err := StringEncoder(qualifiedName.Name)
+	if err != nil {
+		return nil, err
+	}
+	err = binary.Write(buff, binary.LittleEndian, tempBytes)
+	if err != nil {
+		return nil, err
+	}
+	return buff.Bytes(), nil
+}
+
+type SpecialEncoder func(v interface{}) ([]byte, error)
+
+// SpecialStructEncoderMap Such types belong to the built-in types of the OPC UA protocol, but require special adaptation of the coding language.
+var SpecialStructEncoderMap = map[string]SpecialEncoder{
+	"NodeId":          NodeIdEncoder,
+	"ExpandedNodeId":  ExpandedNodeIdEncoder,
+	"ExtensionObject": ExtensionObjectEncoder,
+	"DiagnosticInfo":  DiagnosticInfoEncoder,
+	"LocalizedText":   LocalizedTextEncoder,
+	"DataValue":       DataValueEncoder,
+	"Variant":         VariantEncoder,
+}
+
+func NodeIdEncoder(v interface{}) ([]byte, error) {
+	value, ok := v.(uamsg.NodeId)
+	if !ok {
+		return nil, fmt.Errorf("error type of NodeId, %v", reflect.TypeOf(v).Name())
+	}
+	buff := bytes.NewBuffer(nil)
+	err := binary.Write(buff, binary.LittleEndian, value.EncodingType)
+	if err != nil {
+		return nil, err
+	}
+
+	// node id 使用低4位
+	switch value.EncodingType & 0x0f {
+	case uamsg.TwoByte:
+		identifier, ok := value.Identifier.(byte)
+		if !ok {
+			return nil, fmt.Errorf("two byte nodeId`s identifier has wrong type")
+		}
+		err := binary.Write(buff, binary.LittleEndian, identifier)
+		if err != nil {
+			return nil, err
+		}
+	case uamsg.FourByte:
+		identifier, ok := value.Identifier.(uint16)
+		if !ok {
+			return nil, fmt.Errorf("four byte nodeId`s identifier has wrong type")
+		}
+		err := binary.Write(buff, binary.LittleEndian, byte(value.Namespace))
+		if err != nil {
+			return nil, err
+		}
+		err = binary.Write(buff, binary.LittleEndian, identifier)
+		if err != nil {
+			return nil, err
+		}
+	case uamsg.Numeric:
+		identifier, ok := value.Identifier.(uint32)
+		if !ok {
+			return nil, fmt.Errorf("numeric nodeId has wrong type identifier")
+		}
+		err := binary.Write(buff, binary.LittleEndian, value.Namespace)
+		if err != nil {
+			return nil, err
+		}
+		err = binary.Write(buff, binary.LittleEndian, identifier)
+		if err != nil {
+			return nil, err
+		}
+	case uamsg.String:
+		identifier, ok := value.Identifier.(string)
+		if !ok {
+			return nil, fmt.Errorf("identifier has wrong type, should be string")
+		}
+		err := binary.Write(buff, binary.LittleEndian, value.Namespace)
+		if err != nil {
+			return nil, err
+		}
+		idBytes, err := StringEncoder(identifier)
+		if err != nil {
+			return nil, err
+		}
+		err = binary.Write(buff, binary.LittleEndian, idBytes)
+		if err != nil {
+			return nil, err
+		}
+	case uamsg.GuidType:
+		idBytes, err := GuidEncoder(value.Identifier)
+		if err != nil {
+			return nil, err
+		}
+		err = binary.Write(buff, binary.LittleEndian, value.Namespace)
+		if err != nil {
+			return nil, err
+		}
+		err = binary.Write(buff, binary.LittleEndian, idBytes)
+		if err != nil {
+			return nil, err
+		}
+	case uamsg.ByteString:
+		identifier, ok := value.Identifier.([]byte)
+		if !ok {
+			return nil, fmt.Errorf("bytestring nodeId has wrong type identifier")
+		}
+		err := binary.Write(buff, binary.LittleEndian, value.Namespace)
+		if err != nil {
+			return nil, err
+		}
+		idBytes, err := ByteStringEncoder(identifier)
+		if err != nil {
+			return nil, err
+		}
+		err = binary.Write(buff, binary.LittleEndian, idBytes)
+		if err != nil {
+			return nil, err
+		}
+	default:
+		return nil, fmt.Errorf("nodeId has unknown type identifier")
+	}
+	return buff.Bytes(), nil
+}
+
+func ExpandedNodeIdEncoder(v interface{}) ([]byte, error) {
+	value, ok := v.(uamsg.ExpandedNodeId)
+	if !ok {
+		return nil, fmt.Errorf("error type of ExpandedNodeId, %v", reflect.TypeOf(v).Name())
+	}
+
+	nodeIdBytes, err := NodeIdEncoder(*value.NodeId)
+	if err != nil {
+		return nil, err
+	}
+	buff := bytes.NewBuffer(nil)
+	buff.Write(nodeIdBytes)
+	if (value.EncodingType & uamsg.NamespaceUriFlag) != 0 {
+		nsUriBytes, err := StringEncoder(value.NamespaceUri)
+		if err != nil {
+			return nodeIdBytes, err
+		}
+		err = binary.Write(buff, binary.LittleEndian, nsUriBytes)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if (value.EncodingType & uamsg.ServerIndexFlag) != 0 {
+		err := binary.Write(buff, binary.LittleEndian, value.ServerIndex)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return buff.Bytes(), nil
+}
+
+func ExtensionObjectEncoder(v interface{}) ([]byte, error) {
+	value, ok := v.(uamsg.ExtensionObject)
+	if !ok {
+		return nil, fmt.Errorf("error type of ExtensionObject, %v", reflect.TypeOf(v).Name())
+	}
+	buff := bytes.NewBuffer(nil)
+	typeIdBytes, err := NodeIdEncoder(*value.TypeId)
+	if err != nil {
+		return nil, err
+	}
+	buff.Write(typeIdBytes)
+	buff.WriteByte(value.Encoding)
+	switch value.Encoding {
+	case 0x00:
+	case 0x01:
+		fallthrough
+	case 0x02:
+		bodyBytes, err := StringEncoder(value.Body)
+		if err != nil {
+			return nil, err
+		}
+		bodyLength := int32(len(bodyBytes))
+		err = binary.Write(buff, binary.LittleEndian, bodyLength)
+		if err != nil {
+			return nil, err
+		}
+		buff.Write(bodyBytes)
+	}
+	return buff.Bytes(), nil
+}
+
+func DiagnosticInfoEncoder(v interface{}) ([]byte, error) {
+	value, ok := v.(uamsg.DiagnosticInfo)
+	if !ok {
+		return nil, fmt.Errorf("error type of DiagnosticInfo, %v", reflect.TypeOf(v).Name())
+	}
+	buff := bytes.NewBuffer(nil)
+	buff.WriteByte(value.EncodingMask)
+
+	// todo optimization, chain mode
+	if value.EncodingMask&uamsg.SymbolicIdFlag != 0 {
+		err := binary.Write(buff, binary.LittleEndian, value.SymbolicId)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if value.EncodingMask&uamsg.NamespaceFlag != 0 {
+		err := binary.Write(buff, binary.LittleEndian, value.NamespaceUri)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if value.EncodingMask&uamsg.LocaleFlag != 0 {
+		err := binary.Write(buff, binary.LittleEndian, value.Locale)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if value.EncodingMask&uamsg.LocalizedTextFlag != 0 {
+		err := binary.Write(buff, binary.LittleEndian, value.LocalizedText)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if value.EncodingMask&uamsg.AdditionalInfoFlag != 0 {
+		tempBytes, err := StringEncoder(value.AdditionalInfo)
+		if err != nil {
+			return nil, err
+		}
+		err = binary.Write(buff, binary.LittleEndian, tempBytes)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if value.EncodingMask&uamsg.InnerStatusCodeFlag != 0 {
+		err := binary.Write(buff, binary.LittleEndian, value.InnerStatusCode)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if value.EncodingMask&uamsg.InnerDiagnosticInfoFlag != 0 {
+		dataBytes, err := DiagnosticInfoEncoder(*value.InnerDiagnosticInfo)
+		if err != nil {
+			return nil, err
+		}
+		err = binary.Write(buff, binary.LittleEndian, dataBytes)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return buff.Bytes(), nil
+}
+
+func LocalizedTextEncoder(v interface{}) ([]byte, error) {
+	value, ok := v.(uamsg.LocalizedText)
+	if !ok {
+		return nil, fmt.Errorf("error type of LocalizedText, %v", reflect.TypeOf(v).Name())
+	}
+	buff := bytes.NewBuffer(nil)
+	buff.WriteByte(value.EncodingMask)
+
+	if value.EncodingMask&0x01 != 0 {
+		localeBytes, err := StringEncoder(value.Locale)
+		if err != nil {
+			return nil, err
+		}
+		buff.Write(localeBytes)
+	}
+	if value.EncodingMask&0x02 != 0 {
+		textBytes, err := StringEncoder(value.Text)
+		if err != nil {
+			return nil, err
+		}
+		buff.Write(textBytes)
+	}
+
+	return buff.Bytes(), nil
+}
+
+func DataValueEncoder(v interface{}) ([]byte, error) {
+	value, ok := v.(uamsg.DataValue)
+	if !ok {
+		return nil, fmt.Errorf("error type of DataValue, %v", reflect.TypeOf(v).Name())
+	}
+	buff := bytes.NewBuffer(nil)
+	buff.WriteByte(value.EncodingMask)
+
+	// todo optimization, chain mode
+	if value.EncodingMask&0x01 != 0 {
+		tempBytes, err := VariantEncoder(*value.Value)
+		if err != nil {
+			return nil, err
+		}
+		err = binary.Write(buff, binary.LittleEndian, tempBytes)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if value.EncodingMask&0x02 != 0 {
+		err := binary.Write(buff, binary.LittleEndian, value.ResultStatusCode)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if value.EncodingMask&0x04 != 0 {
+		err := binary.Write(buff, binary.LittleEndian, value.SourceTimestamp)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if value.EncodingMask&0x08 != 0 {
+		err := binary.Write(buff, binary.LittleEndian, value.ServerTimestamp)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if value.EncodingMask&0x10 != 0 {
+		err := binary.Write(buff, binary.LittleEndian, value.SourcePicoSeconds)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if value.EncodingMask&0x20 != 0 {
+		err := binary.Write(buff, binary.LittleEndian, value.ServerPicoSeconds)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return buff.Bytes(), nil
+}
+
+func VariantEncoder(v interface{}) ([]byte, error) {
+	value, ok := v.(uamsg.Variant)
+	if !ok {
+		return nil, fmt.Errorf("error type of DataValue, %v", reflect.TypeOf(v).Name())
+	}
+	buff := bytes.NewBuffer(nil)
+	buff.WriteByte(value.EncodingMask)
+
+	// type-encoding implemented according to the table specified by the OPC UA protocol
+	encodingFn := func(encodingMask byte, v interface{}) ([]byte, error) {
+		tempBuff := bytes.NewBuffer(nil)
+		switch encodingMask & 0x3f {
+		case 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0d, 0x13:
+			err := binary.Write(tempBuff, binary.LittleEndian, v)
+			if err != nil {
+				return nil, err
+			}
+		case 0x0c:
+			tempBytes, err := StringEncoder(v)
+			if err != nil {
+				return nil, err
+			}
+			tempBuff.Write(tempBytes)
+		case 0x0e:
+			idBytes, err := GuidEncoder(v)
+			if err != nil {
+				return nil, err
+			}
+			err = binary.Write(buff, binary.LittleEndian, idBytes)
+			if err != nil {
+				return nil, err
+			}
+		case 0x0f:
+			tempBytes, err := ByteStringEncoder(v)
+			if err != nil {
+				return nil, err
+			}
+			tempBuff.Write(tempBytes)
+		case 0x10:
+			tempBytes, err := StringEncoder(v)
+			if err != nil {
+				return nil, err
+			}
+			tempBuff.Write(tempBytes)
+		case 0x11:
+			tempBytes, err := NodeIdEncoder(v)
+			if err != nil {
+				return nil, err
+			}
+			tempBuff.Write(tempBytes)
+		case 0x12:
+			tempBytes, err := ExpandedNodeIdEncoder(v)
+			if err != nil {
+				return nil, err
+			}
+			tempBuff.Write(tempBytes)
+		case 0x14:
+			tempBytes, err := QualifiedNameEncoder(v)
+			if err != nil {
+				return nil, err
+			}
+			tempBuff.Write(tempBytes)
+		case 0x15:
+			tempBytes, err := LocalizedTextEncoder(v)
+			if err != nil {
+				return nil, err
+			}
+			tempBuff.Write(tempBytes)
+		case 0x16:
+			tempBytes, err := ExtensionObjectEncoder(v)
+			if err != nil {
+				return nil, err
+			}
+			tempBuff.Write(tempBytes)
+		case 0x17:
+			tempBytes, err := DataValueEncoder(v)
+			if err != nil {
+				return nil, err
+			}
+			tempBuff.Write(tempBytes)
+		case 0x18:
+			tempBytes, err := VariantEncoder(v)
+			if err != nil {
+				return nil, err
+			}
+			tempBuff.Write(tempBytes)
+		case 0x19:
+			tempBytes, err := DiagnosticInfoEncoder(v)
+			if err != nil {
+				return nil, err
+			}
+			tempBuff.Write(tempBytes)
+		default:
+			return nil, errors.New("not support variant mask type")
+		}
+		return tempBuff.Bytes(), nil
+	}
+
+	switch {
+	case value.EncodingMask&0xff == 0:
+		// null variant, nothing to do here
+	case value.EncodingMask&0x40 != 0:
+		// multidimensional matrix
+		// when Value stores a multidimensional matrix
+		// ArrayLength stores the number of all elements of the matrix.
+		err := binary.Write(buff, binary.LittleEndian, value.ArrayLength)
+		if err != nil {
+			return nil, err
+		}
+		vv := reflect.ValueOf(value.Value)
+		for i := 0; i < vv.Len(); i++ {
+			for j := 0; j < vv.Index(i).Len(); j++ {
+				tempBytes, err := encodingFn(value.EncodingMask, vv.Index(i).Index(j).Interface())
+				if err != nil {
+					return nil, err
+				}
+				buff.Write(tempBytes)
+			}
+		}
+
+		// ArrayDimensionsLength is the first dimension of the multidimensional matrix
+		err = binary.Write(buff, binary.LittleEndian, value.ArrayDimensionsLength)
+		if err != nil {
+			return nil, err
+		}
+
+		// ArrayDimensions record the second dimension of the multidimensional matrix
+		length := int32(len(value.ArrayDimensions))
+		err = binary.Write(buff, binary.LittleEndian, length)
+		if err != nil {
+			return nil, err
+		}
+		for i := 0; i < int(length); i++ {
+			err := binary.Write(buff, binary.LittleEndian, value.ArrayDimensions[i])
+			if err != nil {
+				return nil, err
+			}
+		}
+	case value.EncodingMask&0x80 != 0:
+		// one dimensional array
+		err := binary.Write(buff, binary.LittleEndian, value.ArrayLength)
+		if err != nil {
+			return nil, err
+		}
+		vv := reflect.ValueOf(value.Value)
+		for i := 0; i < vv.Len(); i++ {
+			tempBytes, err := encodingFn(value.EncodingMask, vv.Index(i).Interface())
+			if err != nil {
+				return nil, err
+			}
+			buff.Write(tempBytes)
+		}
+	default:
+		// basic type
+		tempBytes, err := encodingFn(value.EncodingMask, value.Value)
+		if err != nil {
+			return nil, err
+		}
+		buff.Write(tempBytes)
+	}
+
+	return buff.Bytes(), nil
+}
