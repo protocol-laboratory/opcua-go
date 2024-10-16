3050b731a770dc2f60059431e75c21b0921f56c3
ZhangJian He <shoothzj@gmail.com>
2024-10-16 11:20:22 +0800

    feat: add securechannel handle request
    Signed-off-by: ZhangJian He <shoothzj@gmail.com>

---
+++ opcua/service_set.go
@@ -0,0 +1,27 @@
+package opcua
+
+import "github.com/protocol-laboratory/opcua-go/opcua/uamsg"
+
+func (secChan *SecureChannel) handleOpenSecureChannelServiceRequest(req *uamsg.Message) (*uamsg.Message, error) {
+	return nil, nil
+}
+
+func (secChan *SecureChannel) handleCloseSecureChannelRequest(req *uamsg.Message) (*uamsg.Message, error) {
+	return nil, nil
+}
+
+func (secChan *SecureChannel) handleReadRequest(req *uamsg.Message) (*uamsg.Message, error) {
+	return nil, nil
+}
+
+func (secChan *SecureChannel) handleCreateSessionRequest(req *uamsg.Message) (*uamsg.Message, error) {
+	return nil, nil
+}
+
+func (secChan *SecureChannel) handleActivateSessionRequest(req *uamsg.Message) (*uamsg.Message, error) {
+	return nil, nil
+}
+
+func (secChan *SecureChannel) handleCloseSessionRequest(req *uamsg.Message) (*uamsg.Message, error) {
+	return nil, nil
+}
